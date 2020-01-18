4d1003d4ed2789dd9af09fb71251d99eb5fd096f
syyad <syyadkhan@yahoo.com>
2020-01-18 14:07:44 -0500

    user schema done

--- server/models/user.js
+++ server/models/user.js
@@ -13,6 +13,7 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
+      unique: true,
       trim: true,
       lowercase: true
     },
@@ -26,7 +27,7 @@ const userSchema = new mongoose.Schema(
       default: "subscriber"
     },
     resetPasswordLink: {
-      type: String,
+      data: String,
       default: ""
     }
   },
@@ -34,3 +35,36 @@ const userSchema = new mongoose.Schema(
 );
 
 // virtual
+userSchema
+  .virtual("password")
+  .set(function(password) {
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+//methods
+userSchema.methods = {
+  authenticate: function(plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+  encryptPassword: function(password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+  makeSalt: function() {
+    return Math.round(new Date().valueOf() * Math.random());
+  }
+};
+
+module.exports = mongoose.model("User", userSchema);
